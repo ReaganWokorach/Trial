73f939f91b360442e160557b32dcc9948f60813a
Reagan Wokorach <106845223+ReaganWokorach@users.noreply.github.com>
2024-10-04 23:50:42 +0300

    Practicing pull request

---
+++ index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char name[] = "Reagan Wokorach";
+    int age = 26;
+
+    printf("My name is: %s\n", name);
+    printf("And I am %d years old.\n", age);
+  
+  return 0;
+}
